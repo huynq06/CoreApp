bd627a8151393b4f3102b0cf3638ba747203e0fb
Nguyen Quang Huy <huynq.is@gmail.com>
2021-06-17 02:45:00 +0700

    Create Lesson 11

---
+++ CoreApp.Data/Entities/ProductTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreApp.Data.Enums;
+using CoreApp.Data.Interfaces;
+using CoreApp.Infrastructure.SharedKernel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CoreApp.Data.Entities
+{
+    public class ProductTag : DomainEntity<int>
+    {
+        public int ProductId { get; set; }
+
+        [StringLength(50)]
+        [Column(TypeName = "varchar")]
+        public string TagId { set; get; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { set; get; }
+
+        [ForeignKey("TagId")]
+        public virtual Tag Tag { set; get; }
+    }
+}
---
+++ CoreApp.Data/Interfaces/ISwitchable.cs
@@ -0,0 +1,12 @@
+﻿using CoreApp.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreApp.Data.Interfaces
+{
+    public interface ISwitchable
+    {
+        Status Status { set; get; }
+    }
+}
