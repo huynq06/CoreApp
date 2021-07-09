f5bf2c5d45dfeb52336cce296b48ea62b66d8fc8
Nguyen Quang Huy <huynq.is@gmail.com>
2021-07-09 14:47:53 +0700

    Create Admin Areas

--- CoreApp/Startup.cs
+++ CoreApp/Startup.cs
@@ -103,6 +103,8 @@ namespace CoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "areaRoute",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
             //dbInitializer.Seed().Wait();
         }
