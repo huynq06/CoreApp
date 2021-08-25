674d61ab8fb51ca250715bd4c93bc12146daa2df
Nguyen Quang Huy <huynq.is@gmail.com>
2021-08-25 16:19:29 +0700

    Hiển thị danh mục sản phẩm trên TreeView

---
+++ CoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,32 @@
+﻿using CoreApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        IProductCategoryService _productCategoryService;
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region Get Data API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            int count = model.Count;
+            return new OkObjectResult(model);
+        }
+        #endregion
+    }
+}
